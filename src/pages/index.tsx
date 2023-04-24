9d3e8fc663b554c7cd165be6fb064d2bc8ea4312
Xabier Lameiro <xabier.lameiro@gmail.com>
2023-04-24 18:34:56 +0100

    Save new lint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ import Head from 'next/head';
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Clean started</title>
+                <meta name="description" content="description" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <main>The magic happens here</main>
         </>
     );
