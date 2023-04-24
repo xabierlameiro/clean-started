44b75be321d8d8043d10484d05af59a2f4286e6e
Xabier Lameiro <xabier.lameiro@gmail.com>
2023-04-24 12:12:43 +0100

    Prettier & Eslint config

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 /**
  * curl -X GET http://localhost:3000/api/hello
  */
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Hello, this is the API' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    res.status(200).json({ name: 'Hello, this is the API' });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Clean started</title>
-        <meta name="description" content="description" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        The magic happens here
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Clean started</title>
+                <meta name="description" content="description" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>The magic happens here</main>
+        </>
+    );
 }
