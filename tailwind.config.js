2d98721932863caf615002310a434fe6c1f21724
Xabier Lameiro <xabier.lameiro@gmail.com>
2023-05-11 11:50:34 +0200

    Restore tailwin config by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
     content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
     theme: {
-        colors: {
-            // Custom palette
-        },
         extend: {},
     },
     plugins: [],
