e1ab34322ad74af0424f70c2cf26dca223e296c2
Xabier Lameiro <xabier.lameiro@gmail.com>
2023-05-11 10:47:01 +0200

    Login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,75 @@
+import Image from 'next/image';
 import Head from 'next/head';
 
-export default function Home() {
+export default function Login() {
     return (
         <>
             <Head>
-                <title>Clean started</title>
-                <meta name="description" content="description" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <title>Inicio de sesión</title>
+                <meta name="description" content="Inicio de sesión" />
             </Head>
-            <main data-testid="content">The magic happens here</main>
+            <main data-testid="content">
+                <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <Image src="/logo.png" alt="logo" width={250} height={250} className="mx-auto" />
+                    </div>
+
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6" action="#" method="POST">
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Correo electrónico
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label
+                                        htmlFor="password"
+                                        className="block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        Contraseña
+                                    </label>
+                                    <div className="text-sm">
+                                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                            ¿Has olvidado la contraseña?
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Iniciar sesión
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
