37af24bb5bd2bc52433e3a73fffc94581b538bdc
Sergio Costa <sergio20128@hotmail.com>
2023-05-18 11:44:21 +0200

    test commit - add tw theme menu colors

--- src/pages/plan.tsx
+++ src/pages/plan.tsx
@@ -1,7 +1,10 @@
+import Menu from '../components/menu';
+
 const Employees = ({ user }: { user: string }) => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
             <h1 className="text-2xl font-semibold text-gray-900">Bienvenido, {user}</h1>
+            <Menu />
         </div>
     );
 };
