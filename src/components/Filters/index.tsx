bfc894821d3d2b415a11ea220cda672802836ac5
Sergio Costa <sergio20128@hotmail.com>
2023-05-22 18:09:44 +0200

    fix combined state / funcionality / tw classes

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,110 +1,127 @@
-import React, { useState } from 'react';
-import { InputType } from 'zlib';
+import React, { ChangeEvent, useState } from 'react';
+
+type FilterParams = {
+    documentType: string | undefined;
+    documentNumber: string | undefined;
+    campaign: string | undefined;
+    startDate: string | undefined;
+    endDate: string | undefined;
+    clientName: string | undefined;
+    channelName: string | undefined;
+};
 
 const Filters = () => {
-    //SEARCH INPUT PARAMS
-    //FIX TYPES & COMBINE STATE?
-    const [documentType, setDocumentType] = useState<string>('');
-    const [documentNumber, setDocumentNumber] = useState<InputType>('');
-    const [campaign, setCampaign] = useState<InputType>('');
-    const [startDate, setStartDate] = useState<InputType>('');
-    const [endDate, setEndDate] = useState<InputType>('');
-    const [clientName, setClientName] = useState<InputType>('');
-    const [channelName, setChannelName] = useState<InputType>('');
+    //FIX TYPES & COMBINED STATE?
+    const [filterParams, setFilterParams] = useState<FilterParams>({
+        documentType: '',
+        documentNumber: '',
+        campaign: '',
+        startDate: '',
+        endDate: '',
+        clientName: '',
+        channelName: '',
+    });
+    const { documentType, documentNumber, campaign, startDate, endDate, clientName, channelName } = filterParams;
 
-    // HANDLE FUCTION FOR COMBINE STATE?
+    // HANDLE FUCTION FOR COMBINE STATE
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+
+        setFilterParams((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
     // HANDLE SEARCH FUNCTION/LOGIC
+    const handleSearch = () => {};
 
+    // DISABLED WHEN FILE TYPE NOT SELECTED
     return (
-        <div className="flex flex-col">
-            <div className="flex items-center">
-                <div className="mr-4 w-32">
-                    <h3 className="text-xl font-bold">Documento</h3>
-                </div>
-                <div className="mr-4">
+        <section className="p-4">
+            <div className="flex flex-col w-full gap-4">
+                <div className="flex items-center gap-4">
+                    <h3 className="w-20 font-bold">Documento</h3>
                     <select
-                        className="w-40 px-2 py-1 font-semibold border-2 rounded"
+                        className="w-24 px-2 py-1 font-semibold border-2 rounded"
+                        name="documentType"
                         value={documentType}
-                        onChange={(e) => setDocumentType(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                     >
                         <option value="">Tipo</option>
                         <option value="txt">.txt</option>
                         <option value="xls">.xls</option>
                         <option value="doc">.doc</option>
                     </select>
-                </div>
-                <div className="mr-4">
                     <input
                         type="text"
                         placeholder="Nº Documento"
-                        className="w-40 px-2 py-1 border rounded"
+                        className="w-32 px-2 py-1 border rounded"
+                        name="documentNumber"
                         value={documentNumber}
-                        onChange={(e) => setDocumentNumber(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
-                </div>
-                <div className="mr-4">
                     <input
                         type="text"
                         placeholder="Campaña"
-                        className="w-[400px] px-2 py-1 border rounded"
+                        className="w-1/3 px-2 py-1 border rounded"
+                        name="campaign"
                         value={campaign}
-                        onChange={(e) => setCampaign(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
-                </div>
-                <div className="mr-4">
                     <input
                         type="date"
                         placeholder="Fecha Contable Desde"
                         className="w-40 px-2 py-1 border rounded"
+                        name="startDate"
                         value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
-                </div>
-                <div>
                     <input
                         type="date"
                         placeholder="Fecha Contlable Hasta"
                         className="w-40 px-2 py-1 border rounded"
+                        name="endDate"
                         value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
                 </div>
-            </div>
-            <div className="flex items-center mt-4">
-                <div className="mr-4 w-32">
-                    <h3 className="text-xl font-bold">Cliente</h3>
-                </div>
-                <div className="mr-4">
+                <div className="flex items-center gap-4">
+                    <h3 className="w-20 font-bold">Cliente</h3>
                     <input
                         type="text"
                         placeholder="Nombre"
-                        className="w-[400px] px-2 py-1 border rounded"
+                        className="w-full px-2 py-1 border rounded md:w-2/3"
+                        name="clientName"
                         value={clientName}
-                        onChange={(e) => setClientName(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
                 </div>
-            </div>
-            <div className="flex items-center mt-4">
-                <div className="mr-4 w-32">
-                    <h3 className="text-xl font-bold">Canal</h3>
-                </div>
-                <div className="mr-4">
+                <div className="flex items-center gap-4 sm">
+                    <h3 className="w-20 font-bold">Canal</h3>
                     <input
                         type="text"
                         placeholder="Nombre del Canal"
-                        className="w-[400px] px-2 py-1 border rounded"
+                        className="w-full  px-2 py-1 border rounded md:w-2/3 "
+                        name="channelName"
                         value={channelName}
-                        onChange={(e) => setChannelName(e.target.value)}
+                        onChange={(e) => handleInputChange(e)}
                         disabled={!documentType}
                     />
                 </div>
+                <button
+                    className="w-full h-10 bg-green-500 text-white font-bold rounded hover:bg-green-400"
+                    onClick={handleSearch}
+                >
+                    Buscar
+                </button>
             </div>
-        </div>
+        </section>
     );
 };
 
