bd6709cf6a780d42320384571b1bc715aa01c011
Gabi <gaby.ballester@gmail.com>
2023-05-22 17:03:33 +0200

    feature/layout: Apply index path to key on map

--- src/components/Header/components/Dropdown/components/DropdownMenu.tsx
+++ src/components/Header/components/Dropdown/components/DropdownMenu.tsx
@@ -4,10 +4,10 @@ import { itemsList } from '@/mocks/mockDropdownHeaderList';
 export const DropdownMenu = () => {
     return (
         <div className="bg-white border-2 absolute right-5 top-12 flex flex-col items-start rounded-lg p-4 gap-2 w-60">
-            {itemsList.map(({ label, path }, index) => (
-                <div key={index}>
-                    <Link className="text-sm hover:font-medium cursor-pointer" href={path}>
-                        {label}
+            {itemsList.map((item, index) => (
+                <div key={index + item.path}>
+                    <Link className="text-sm hover:font-medium cursor-pointer" href={item.path}>
+                        {item.label}
                     </Link>
                 </div>
             ))}
