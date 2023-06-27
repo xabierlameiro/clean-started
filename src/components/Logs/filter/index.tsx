307a9b609f93c6d252a33369019cf97de7db754c
Sergio Costa <sergio20128@hotmail.com>
2023-06-27 14:14:17 +0200

    changed log calendarFilter to filter

---
+++ src/components/Logs/filter/index.tsx
@@ -0,0 +1,34 @@
+const LogsFilter: React.FC = () => {
+    return (
+        <section className="w-full mb-4 border-2 shadow-lg">
+            <div className="flex items-center justify-end gap-4">
+                <input
+                    type="text"
+                    placeholder="Nº Plan"
+                    className={`text-md px-2 py-1 border rounded w-32 flex-grow `}
+                    name="documentNumber"
+                    value=""
+                />
+
+                <input
+                    type="text"
+                    placeholder="Nombre del Plan"
+                    className={`px-2 py-1 border rounded w-32 flex-grow `}
+                    name="campaign"
+                    value=""
+                />
+                <div className="flex items-center gap-2">
+                    <label title="end date">Desde</label>
+                    <input name="start date" type="date" className="text-sm px-2 py-1 border rounded w-40" />
+                </div>
+                <div className="flex items-center gap-2">
+                    <label title="end date">hasta</label>
+                    <input name="end date" type="date" className="text-sm px-2 py-1 border rounded w-40" />
+                </div>
+                <button className="bg-green-500 text-white w-24 rounded-md">Filtrar</button>
+            </div>
+        </section>
+    );
+};
+
+export default LogsFilter;
